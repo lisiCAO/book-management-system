0815e117234272dc7b15ec3b8c8eb3715f77e895
lisiCAO <caolisi@hotmail.com>
2023-12-03 09:46:40 -0500

    create microservices

---
+++ book-management-system/discovery-service/src/test/java/com/fsd/bookmanagementsystem/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsd.bookmanagementsystem.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
